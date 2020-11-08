8122cd0508ccaa0a20e21e528bc48ea1a1d1cecc
Jonathan Lee <jonathan-lee@berkeley.edu>
2020-11-07 22:28:08 -0500

    Started work on Ansible provisioning playbook

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dotfiles=$(git rev-parse --show-toplevel)
-
-sudo cp $dotfiles/systemd/* /etc/systemd/system
-sudo systemctl enable rkhunter.timer
-sudo systemctl start rkhunter.timer
