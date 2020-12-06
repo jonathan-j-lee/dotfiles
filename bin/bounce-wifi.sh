bcb0c413f6b069da21af0456c45712d8991d2160
Jonathan Lee <jonathan-lee@berkeley.edu>
2020-12-06 00:02:43 -0500

    Added task for disabling GNOME tracker

--- bin/bounce-wifi.sh
+++ bin/bounce-wifi.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# Sometimes, wifi will stop working after suspension (XPS 15, Arch Linux). Example dmesg errors:
+# Script for replugging the Broadcom wifi kernel module when it fails.
+#
+# Example dmesg entries:
 #
 #   ieee80211 phy0: brcmf_msgbuf_tx_ioctl: Failed to reserve space in commonring
 #   ieee80211 phy0: brcmf_cfg80211_set_power_mgmt: error (-12)
 #
-# This script reloads a wifi-related kernel module.
+# This issue is under investigation: https://bugzilla.kernel.org/show_bug.cgi?id=201853
+#
+# Machine: XPS 15, Linux 5.x
 
 kernel_version=$(ls -1 /lib/modules | tail -1)
 echo "Kernel version: ${kernel_version}"
@@ -13,5 +17,8 @@ echo "Kernel version: ${kernel_version}"
 module=brcmfmac
 set -x
 modprobe -r ${module}
+# By default, modprobe infers the kernel version from `uname`, which fails if
+# the kernel headers are more up-to-date than the live kernel. Therefore, we
+# explicitly supply the kernel version.
 modprobe -S ${kernel_version} ${module}
 systemctl restart NetworkManager.service
