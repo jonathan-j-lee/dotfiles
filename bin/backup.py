8d39eff31dc8c097ffe8ddc340b4d2cfb223dbb1
Jonathan Lee <jonathan-lee@berkeley.edu>
2020-12-13 16:05:39 -0500

    Working recursive hash and merge

--- bin/backup.py
+++ bin/backup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
+import collections.abc
+import contextlib
 import dataclasses
 import datetime
+import functools
 import hashlib
+import io
 import itertools
 import json
 import pathlib
+import queue
 import tarfile
 import typing
 import re
@@ -14,7 +19,7 @@ import click
 
 
 METADATA_NAME = '.backup'
-KEBIBYTE, MEBIBYTE, GIBIBYTE = 2**10, 2**20, 2**30
+KIBIBYTE, MEBIBYTE, GIBIBYTE = 2**10, 2**20, 2**30
 
 as_path = lambda _ctx, path: pathlib.Path(path)
 as_paths = lambda _ctx, paths: tuple(map(pathlib.Path, paths))
@@ -48,13 +53,13 @@ class ProgressBar:
 
     def humanize_file_size(self, byte_count: int, rounding: int = 1) -> str:
         prefixes = ['', 'Ki', 'Mi', 'Gi']
-        sizes = [1, KEBIBYTE, MEBIBYTE, GIBIBYTE, float('inf')]
+        sizes = [1, KIBIBYTE, MEBIBYTE, GIBIBYTE, float('inf')]
         for prefix, divisor, limit in zip(prefixes, sizes[:-1], sizes[1:]):
             if byte_count < limit:
                 value = byte_count if divisor == 1 else round(byte_count/divisor, rounding)
                 return f'{value} {prefix}B'
 
-    def update_rate(self, info, default: float = KEBIBYTE) -> float:
+    def update_rate(self, info, default: float = KIBIBYTE) -> float:
         """ Computes the archive rate in bytes per second. """
         now = datetime.datetime.now()
         delta, self.last_update = now - self.last_update, now
@@ -109,6 +114,115 @@ class ProgressBar:
 
 
 @dataclasses.dataclass(frozen=True)
+class FileHashTree(collections.abc.Mapping[pathlib.Path, 'FileHashTree']):
+    info: typing.Optional[tarfile.TarInfo] = None
+    digest: bytes = b''
+    children: typing.MutableMapping[str, 'FileHashTree'] = dataclasses.field(default_factory=dict)
+
+    ROOT: typing.ClassVar[pathlib.Path] = pathlib.Path('/')
+
+    @classmethod
+    def _normalize_path(cls, path: typing.Union[str, pathlib.Path]) -> pathlib.Path:
+        if isinstance(path, str):
+            path = pathlib.Path(path)
+        return path.relative_to(cls.ROOT) if path.is_absolute() else path
+
+    @staticmethod
+    def _digest(source: typing.Union[bytes, bytearray, io.FileIO],
+                chunk_size: int = 10*KIBIBYTE, hash_alg: str = 'sha256') -> bytes:
+        make_hash = getattr(hashlib, hash_alg)
+        if isinstance(source, (bytes, bytearray)):
+            hash_obj = make_hash(source)
+        else:
+            hash_obj, buf = make_hash(), bytearray(chunk_size)
+            while source.readinto(buf):
+                hash_obj.update(buf)
+        return hash_obj.digest()
+
+    def __getitem__(self, path: typing.Union[str, pathlib.Path]) -> 'FileHashTree':
+        path = self._normalize_path(path)
+        if not path.parts:
+            return self
+        name, *remainder = path.parts
+        return self.children[name][pathlib.Path().joinpath(*remainder)]
+
+    def __iter__(self):
+        yield self
+        for part, child in sorted(self.children.items()):
+            yield from child
+
+    def __len__(self) -> int:
+        return bool(self.info) + sum(map(len, self.children))
+
+    @classmethod
+    def scan(cls, tar: tarfile.TarFile, path: pathlib.Path,
+             base: typing.Optional[pathlib.Path] = None) -> typing.Optional['FileHashTree']:
+        base = base or path
+        with contextlib.suppress(PermissionError):
+            if not path.exists() or not (info := tar.gettarinfo(path)):
+                return
+            # TODO: add exclude
+            node = FileHashTree(info)
+            if path.is_dir():
+                for child_path in path.iterdir():
+                    if child := FileHashTree.scan(tar, child_path, base=base):
+                        node.children[child_path.name] = child
+            if base == path:
+                for part in reversed(base.relative_to(cls.ROOT).parts):
+                    node = FileHashTree(children={part: node})
+            return node
+
+    @property
+    def size(self):
+        size = self.info.size if self.info else 0
+        return size + sum(child.size for child in self.children.values())
+
+    def with_digests(self, path: pathlib.Path = ROOT, hook=None,
+                     hash_alg: str = 'sha256', sep: bytes = b'|') -> 'FileHashTree':
+        children = {part: child.with_digests(path.joinpath(part), hook, hash_alg, sep)
+                    for part, child in sorted(self.children.items())}
+        digest = functools.partial(self._digest, hash_alg=hash_alg)
+        info_digest = content_digest = b''
+        if self.info:
+            info_digest = digest(self.info.tobuf())
+            if self.info.isfile():
+                with open(path, 'rb') as file_handle:
+                    content_digest = digest(file_handle)
+                    # File cache has been warmed up, so re-reading should not be too slow.
+                    if hook:
+                        file_handle.seek(0)
+                        hook(self.info, file_handle)
+            elif hook:
+                hook(self.info)
+        digests = [info_digest, content_digest] + [child.digest for child in children.values()]
+        return FileHashTree(self.info, digest(sep.join(digests)), children)
+
+    def __or__(self, other: typing.Optional['FileHashTree']) -> 'FileHashTree':
+        """ Merge two file hash trees. The latest (right) operand takes precedence. """
+        if not other:
+            return self
+        children = {part: child for part, child in self.children.items()
+                    if part not in other.children}
+        for part, child in other.children.items():
+            children[part] = child | self.children.get(part)
+        return FileHashTree(other.info or self.info, other.digest or self.digest, children)
+
+    __ror__ = __or__
+
+#     def create_archive(self, tar: tarfile.TarFile, bar: ProgressBar):
+#         def archive_hook(info, file_handle: typing.Optional[io.FileIO] = None):
+#             # File cache has been warmed up, so rereading should not be too slow.
+#             tar.addfile(info, file_handle)
+#             if info.size > 0:
+#                 bar.update(info)
+#         self.update_hashes(archive_hook)
+
+
+def exclude():
+    pass
+
+
+@dataclasses.dataclass(frozen=True)
 class Archive:
     DEFAULT_EXCLUDE = tuple(map(re.compile, [
         r'^__pycache__$',
@@ -122,19 +236,13 @@ class Archive:
     ]))
 
     tar: tarfile.TarFile
+    hashes: dict = dataclasses.field(default_factory=dict, init=False)
     hashing_algorithm: str = 'sha256'
     file_types: typing.Sequence[str] = dataclasses.field(default_factory=list)
     exclude: typing.Sequence[re.Pattern] = dataclasses.field(default=DEFAULT_EXCLUDE)
 
     def should_index_special(self, info: tarfile.TarInfo) -> bool:
         return any(getattr(info, f'is{file_type}')() for file_type in self.file_types)
-
-    def hash_file(self, file_handle, chunk_size: int = 10*KEBIBYTE) -> str:
-        """ Compute the hash of a file. """
-        hash = getattr(hashlib, self.hashing_algorithm)()
-        while (buf := file_handle.read(chunk_size)):
-            hash.update(buf)
-        return hash.hexdigest()
 
     def should_exclude(self, path: pathlib.Path) -> bool:
         return any(pattern.search(path.name) for pattern in self.exclude)
@@ -151,21 +259,6 @@ class Archive:
                 yield from self.scan(child)
         elif info.isfile() or self.should_index_special(info):
             yield info
-
-    def create(self, infos: typing.Iterable[tarfile.TarInfo], bar: ProgressBar):
-        root = pathlib.Path('/')
-        for info in infos:
-            if info.isfile():
-                path = root.joinpath(info.name)
-                with open(path, 'rb') as file_handle:
-                    hash = self.hash_file(file_handle)
-                    # The file cache has been warmed up, so re-reading the file
-                    # should not be too slow.
-                    file_handle.seek(0)
-                    self.tar.addfile(info, file_handle)
-                    bar.update(info)
-            else:
-                self.tar.addfile(info)
 
 
 def filter_redundant_sources(sources: typing.Sequence[pathlib.Path]) \
@@ -216,13 +309,23 @@ def create(ctx, **options):
 
     sources = tuple(filter_redundant_sources(options['source']))
     with tarfile.open(str(ctx.obj['archive'] / filename), 'w|' + compression) as tar:
-        archive = Archive(tar, options['hash'], options['type'])
-        infos = list(itertools.chain(*(archive.scan(source) for source in sources)))
-        for info in infos:
-            print(info.name)
-        total_bytes = sum(info.size for info in infos)
-        with ProgressBar(total_bytes) as bar:
-            archive.create(infos, bar)
+        root = None
+        for source in sources:
+            root |= FileHashTree.scan(tar, source)
+        root = root.with_digests()
+        print(root.digest)
+        for x in root:
+            print(x.info)
+        # node = root['/home/jonathanlee/uc-berkeley/2016-fall']
+        # node.update_hash_digests(hash_alg=options['hash'])
+        # print(node.digest)
+
+        # archive = Archive(tar, options['hash'], options['type'])
+        # infos = list(itertools.chain(*(archive.scan(source) for source in sources)))
+        # total_bytes = sum(info.size for info in infos)
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        #     with ProgressBar(total_bytes) as bar:
+        #         archive.create(infos, bar, executor)
 
 
 @cli.command()
